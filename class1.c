f00acd6b0d5ab2f2348216b34a6e9afe6ef64af7
nopputhlinna <106652056+nopputhlinna@users.noreply.github.com>
2023-12-19 10:35:36 +0700

    Update class1.c

--- class1.c
+++ class1.c
@@ -4,6 +4,7 @@ int main(){
     char string[]= "Hello: I am from Cambodia";
     size_t length = strlen(string);
 
-    printf("Length of the string : %zu\n", length);
+    printf("Length of the string : %zu\n", length);]
+    printf("Hello guys!");
     return 0;
 }

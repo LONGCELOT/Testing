8cb9ac70a0fc5cac3416664a7b1b64d06b08ad06
CheaLivchea <153539077+CheaLivchea@users.noreply.github.com>
2023-12-19 10:34:01 +0700

    Add files via upload

---
+++ class1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char string[]= "Hello: I am from Cambodia";
+    size_t length = strlen(string);
+
+    printf("Length of the string : %zu\n", length);
+    return 0;
+}
